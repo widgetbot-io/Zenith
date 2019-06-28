62a5863582aa04e913795c86c1de99e75d87246e
Daave <daave@aaathats3as.com>
2019-06-28 23:30:36 +0100

    Event loading and example event

---
+++ src/Classes/EventLoader.ts
@@ -0,0 +1,36 @@
+import {Client} from '../Client';
+import * as _cliProgress from 'cli-progress';
+import {sync} from 'glob';
+
+export class EventLoader {
+    constructor(private Client: Client) { }
+    getLoadableEvents(): string[] {
+        let files: string[] = sync(`${__dirname}/../Events/**/*.**`);
+        files = files.filter(a => !a.endsWith('.d.ts') || !a.endsWith('.map'));
+
+        return files;
+    }
+
+    async loadEvents(): Promise<void> {
+        let start: number = 0;
+        const progressBar = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic);
+        const events: string[] = await this.getLoadableEvents();
+
+        console.log('Loading Events...');
+        progressBar.start(events.length, start);
+        for (const event of events) {
+            start += 1;
+            await progressBar.update(start);
+            await require(event);
+        }
+
+        await progressBar.stop();
+    }
+
+    async digestEvents() {
+        console.log('hi');
+        for (const event of Client.events) {
+            console.log(event);
+        }
+    }
+}
---
+++ src/Events/ready.ts
@@ -0,0 +1,9 @@
+import { Event } from "../services/decorators";
+
+@Event({
+	name: 'ready',
+	description: 'haha'
+})
+export class Ready {
+
+}
