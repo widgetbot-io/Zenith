f2c89504c952eb51b51c2f93c99a601f1dd2f5f5
Daave <daave@aaathats3as.com>
2019-06-28 18:46:30 +0100

    Use correct import

--- src/Classes/CommandHelper.ts
+++ src/Classes/CommandHelper.ts
@@ -1,6 +1,6 @@
 import {Message} from "discord.js";
 import {Client} from "../Client";
-import {Module} from "../services/decorators";
+import {Module} from "../interfaces";
 
 export class CommandHelper {
     constructor(public message: Message, public client: Client, public module: Module, public argHelper: any) {}
