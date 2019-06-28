7a7945620866f4f8620dc117dc39fd5a144087a2
Daave <daave@aaathats3as.com>
2019-06-28 21:13:41 +0100

    Add gets to helper

--- src/Classes/CommandHelper.ts
+++ src/Classes/CommandHelper.ts
@@ -5,12 +5,20 @@ import {Module} from "../interfaces";
 export class CommandHelper {
     constructor(public message: Message, public client: Client, public module: Module, public argHelper: any) {}
 
-    async send(content: string): Promise<Message> {
+    public get channel() {
+        return this.message.channel;
+    }
+
+    public get guild() {
+        return this.message.guild;
+    }
+
+    public async send(content: string): Promise<Message> {
         let message;
 
         if (this.client.commandHandler.ranCommands[this.message.id]) {
             const old: Message = this.client.commandHandler.ranCommands[this.message.id];
-            message = old.edit(content);
+            message = await old.edit(content);
         } else {
             message = await this.message.channel.send(content);
         }
