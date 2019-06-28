04ced17698ef721ca2dac5ada760e89e3a786102
Daave <daave@aaathats3as.com>
2019-06-28 18:47:40 +0100

    Basic helper send function

--- src/Classes/CommandHelper.ts
+++ src/Classes/CommandHelper.ts
@@ -4,4 +4,8 @@ import {Module} from "../interfaces";
 
 export class CommandHelper {
     constructor(public message: Message, public client: Client, public module: Module, public argHelper: any) {}
+
+    async send(content: string): Promise<Message | Message[]> {
+        return await this.message.channel.send(content);
+    }
 }
