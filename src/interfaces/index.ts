41609651f053f4ddfebc48b266c4e2cbaed89cba
Daave <daave@aaathats3as.com>
2019-06-30 16:29:06 +0100

    Remove old export

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,5 +2,5 @@ export { Base, Command, Module, Event } from './decorators';
 export { BaseCommand, BaseEvent } from './command';
 export { Limit, LimitSettings, RatelimitType } from './ratelimit';
 export { Options } from './client';
-export { ArgumentType, BaseArgumentOptions } from './arguments';
+export { BaseArgumentOptions } from './arguments';
 export {  } from './decorators';
