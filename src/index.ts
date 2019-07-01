9322cd1ef2ceb7bf8420759b876d754f0654d649
Daave <daave@aaathats3as.com>
2019-07-01 21:52:25 +0100

    Export Bot

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ICommand, IEvent, IModule, BaseArgumentOptions, Base, BaseEvent, RatelimitType, LimitSettings, Limit, BaseCommand, Options} from './interfaces';
 export { BaseArgument, FlagArgumentWithValue, FlagArgument, RequiredArgument, OptionalArgument, BaseLoader, CommandLoader, CommandHelper } from './Classes';
 export { Module, Event, Command } from './services/decorators';
+export { Bot } from'./Bot';
