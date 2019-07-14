edba071b23a6b68f42c2e23727a072b520458b24
Daave <daave@aaathats3as.com>
2019-07-14 17:59:36 +0100

    Export logger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { ICommand, IEvent, IModule, BaseArgumentOptions, Base, BaseEvent, RatelimitType, LimitSettings, Limit, BaseCommand, Options} from './interfaces';
-export { BaseArgument, FlagArgumentWithValue, FlagArgument, RequiredArgument, OptionalArgument, BaseLoader, CommandLoader, CommandHelper } from './Classes';
+export { BaseArgument, FlagArgumentWithValue, FlagArgument, RequiredArgument, OptionalArgument, BaseLoader, CommandLoader, CommandHelper, Logger } from './Classes';
 export { Module, Event, Command } from './services/decorators';
 export { Bot } from'./Bot';
